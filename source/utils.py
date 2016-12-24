abda551ab7b6bd4075a92a740b5839feee645a36
Michael <645092284@qq.com>
2016-12-24 21:05:05 +0800

    gitPython看起来还不错，但是网太差！

--- source/utils.py
+++ source/utils.py
@@ -2,8 +2,9 @@
 # @Author: Michael
 # @Date:   2016-12-24 03:34:39
 # @Last Modified by:   Michael
-# @Last Modified time: 2016-12-24 18:51:14
+# @Last Modified time: 2016-12-24 21:04:12
 import os
+import git
 from .exceptions import ResorceNotFoundError
 
 
@@ -14,3 +15,16 @@ class GitEngine(object):
     def __init__(self, root):
         super(GitEngine, self).__init__()
         self.repo = None
+        self.root = root
+
+    def setup(self):
+        self.repo = git.Repo.init()
+
+    def lookup(self):
+        self.repo = git.Repo(self.root)
+
+    def add(self, owner, url):
+        self.repo.create_submodule(owner, owner, url=url)
+
+    def update(self, owner, url):
+        pass
