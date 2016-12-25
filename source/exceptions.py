77c1d94353339ae4e0fc43b2602bdb2e2b5a0b21
Michael <645092284@qq.com>
2016-12-25 23:39:43 +0800

    gitpython貌似有毒，尝试找到一个合适的解决方案，但是还没成功

--- source/exceptions.py
+++ source/exceptions.py
@@ -2,7 +2,7 @@
 # @Author: Michael
 # @Date:   2016-12-24 01:40:08
 # @Last Modified by:   Michael
-# @Last Modified time: 2016-12-24 06:00:38
+# @Last Modified time: 2016-12-25 21:16:16
 
 
 class InvalidValueError(Exception):
--- source/utils.py
+++ source/utils.py
@@ -2,10 +2,8 @@
 # @Author: Michael
 # @Date:   2016-12-24 03:34:39
 # @Last Modified by:   Michael
-# @Last Modified time: 2016-12-24 21:04:12
-import os
+# @Last Modified time: 2016-12-25 23:38:14
 import git
-from .exceptions import ResorceNotFoundError
 
 
 class GitEngine(object):
@@ -14,17 +12,34 @@ class GitEngine(object):
     """
     def __init__(self, root):
         super(GitEngine, self).__init__()
-        self.repo = None
         self.root = root
+        self.problemHub = git.Repo(root)
+        self.ownerRepos = self.problemHub.submodules
+
+    def updateOwners(self):
+        if self.problemHub is None:
+            raise UnboundLocalError("didn't init repo.")
+        else:
+            self.owners = self.problemHub.submodules
 
     def setup(self):
-        self.repo = git.Repo.init()
+        self.problemHub = git.Repo.init()
 
     def lookup(self):
-        self.repo = git.Repo(self.root)
+        self.problemHub = git.Repo(self.root)
 
-    def add(self, owner, url):
-        self.repo.create_submodule(owner, owner, url=url)
+    def addOnwerRepo(self, owner, url):
+        self.problemHub.create_submodule(owner, owner, url=url)
 
-    def update(self, owner, url):
-        pass
+    def update(self, owner):
+        ownerRepo = git.submodule(owner)
+        ownerRepo.update(to_latest_revision=True)
+        self.problemHub.git.add(u=True)
+        self.problemHub.index.commit("%s from %s is updated." % owner, ownerRepo.url)
+
+#
+# after all night test..... I got a conclusion is that the package gitpython is poisonous.
+# it always add more untracked changes.
+# 
+# have to explore more to find a wat out using libgit2 which means pygit2.
+#
